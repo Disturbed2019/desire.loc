22945254a1660d5e344e93b392f6a9badb137067
Disturbed2019 <disturbe@bk.ru>
2021-06-04 12:40:02 +0300

    First

---
+++ app/js/main.js
@@ -0,0 +1,53 @@
+// $(function () {
+//
+// })
+document.addEventListener("DOMContentLoaded",function(){
+const openBtn = document.querySelector('.header__btn');
+const closeBtn = document.querySelector('.rightside-menu__close');
+const menu = document.querySelector('.rightside-menu');
+const openMenu = () =>{
+    menu.classList.remove('rightside-menu--close');
+};
+const closeMenu = () =>{
+    menu.classList.add('rightside-menu--close')
+};
+    openBtn.addEventListener('click', openMenu);
+    closeBtn.addEventListener('click', closeMenu);
+
+    const slider = document.querySelector('.top__slider');
+    const contact_slider = document.querySelector('.contact-slider');
+    const article_slider = document.querySelector('.article-slider__box');
+    if (slider || contact_slider || article_slider){
+        $('.top__slider').slick({
+            dots: true,
+            arrows:false,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+        $('.contact-slider').slick({
+            dots: true,
+            arrows:false,
+            slidesToShow: 10,
+            slidesToScroll:10
+        });
+        $('.article-slider__box').slick({
+            dots: false,
+            arrows:true,
+            prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/icons/arrow-slider-left.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/icons/arrow-slider-right.svg" alt=""></button>'
+        });
+
+    }
+
+    let mix = document.querySelector('.gallery__inner');
+    if (mix){
+        let Mixer = mixitup ( '.gallery__inner',{
+            load: {
+                filter: '.bedroom'
+            }
+        } )
+    }
+
+
+});
